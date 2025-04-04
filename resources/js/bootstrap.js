687e9cef4889ffdb54da16437148331f000f5fa8
Vun Thy <Vun.Thy@mjqe.edu.kh>
2025-04-04 17:39:19 +0700

    ClearSt

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,8 +8,6 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
