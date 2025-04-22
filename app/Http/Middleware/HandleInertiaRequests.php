2c3bfdacaf60535c57620e2672bec9bfdcdaec48
Vun Thy <Vun.Thy@mjqe.edu.kh>
2025-04-22 13:45:51 +0700

    Update

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -32,7 +32,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user() ? $request->user()->only(['id', 'name', 'profile', 'email', 'signature',]) : null,
+                'user' => $request->user(),
             ],
         ];
     }
